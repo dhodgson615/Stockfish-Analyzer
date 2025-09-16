63178a4370d966175f783de63223b3009bd40fbd
dhodgson615 <dhodgson0615@gmail.com>
2025-09-16 19:43:54 -0400

    Removed variable assignment and just returned the object directly

--- conftest.py
+++ conftest.py
@@ -23,8 +23,7 @@ def checkmate_board() -> Board:
 @pytest.fixture
 def stalemate_board() -> Board:
     """Returns a board with a stalemate position."""
-    board = Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
-    return board
+    return Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
 
 
 @pytest.fixture
