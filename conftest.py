9792da2cecc959940ec995b04f95f4bcc0484459
dhodgson615 <dhodgson0615@gmail.com>
2025-09-16 19:43:08 -0400

    Specified function return type

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ def checkmate_board() -> Board:
 
 
 @pytest.fixture
-def stalemate_board():
+def stalemate_board() -> Board:
     """Returns a board with a stalemate position."""
     board = Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
     return board
