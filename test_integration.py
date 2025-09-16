ab093877b64ff761f2b91aeff5b1d88c7c40cf55
dhodgson615 <dhodgson0615@gmail.com>
2025-09-16 19:09:12 -0400

    Added TODO comment

--- test_integration.py
+++ test_integration.py
@@ -9,7 +9,8 @@ from chess.engine import SimpleEngine
 from input_handler import from_uci
 from game_logic import play_game
 
-
+"""TODO: Consider removing this because there is no point in running
+tests without the engine"""
 @pytest.mark.skipif(
     not path.exists("/opt/homebrew/bin/stockfish"),
     reason="Stockfish engine not found",
