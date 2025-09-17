cb3f7eae2203dffa890f240765e6b1bc12935e84
dhodgson615 <dhodgson0615@gmail.com>
2025-09-17 17:25:27 -0400

    Added Tablebase type

--- engine_handler.py
+++ engine_handler.py
@@ -13,7 +13,7 @@ ENGINE_PATH = "/opt/homebrew/bin/stockfish"
 SYZYGY_PATH = path.expanduser("~/chess/syzygy")
 
 
-def get_syzygy_tablebase(filepath=SYZYGY_PATH) -> object | None:
+def get_syzygy_tablebase(filepath=SYZYGY_PATH) -> Tablebase | None:
     """Initialize a Syzygy tablebase."""
     if not path.exists(filepath):
         print(f"Syzygy tablebases not found at {filepath}")
