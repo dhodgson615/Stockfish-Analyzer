a992d5b26734d164eafb79754ff5733a63369cd4
dhodgson615 <dhodgson0615@gmail.com>
2025-09-17 15:41:42 -0400

    Added return type annotation

--- test_input_handler.py
+++ test_input_handler.py
@@ -47,7 +47,7 @@ def new_board():
     return Board()
 
 
-def test_handle_user_input_valid_move(monkeypatch):
+def test_handle_user_input_valid_move(monkeypatch) -> None:
     """Test handle_user_input with valid input."""
     from input_handler import handle_user_input
 
