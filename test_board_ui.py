cf90636188eb01db848d0e36367aaccfa73abed7
dhodgson615 <dhodgson0615@gmail.com>
2025-09-18 13:41:21 -0400

    Spacing

--- test_board_ui.py
+++ test_board_ui.py
@@ -87,6 +87,7 @@ def test_show_mate_info() -> None:
         move,
         (100, None),
     )
+
     with StringIO() as buf, redirect_stdout(buf):
         show_mate_info(best_move_data_no_mate, True)
         assert buf.getvalue() == ""
