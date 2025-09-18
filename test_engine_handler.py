03a15da2a3f63c4a144b20c820c58071a2e88109
dhodgson615 <dhodgson0615@gmail.com>
2025-09-18 13:47:08 -0400

    Spacing

--- test_engine_handler.py
+++ test_engine_handler.py
@@ -194,6 +194,7 @@ def test_get_engine_evaluation() -> None:
     mock_score.white.return_value.mate.return_value = None
     mock_engine.analyse.return_value = {"score": mock_score}
     score, mate = get_engine_evaluation(board, mock_engine, EVAL_DEPTH)
+
     assert score == 42
     assert mate is None
 
