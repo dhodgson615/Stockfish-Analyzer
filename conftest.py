6d98f4597cb752ff6b59d769c1691f46c86cf185
dhodgson615 <dhodgson0615@gmail.com>
2025-09-17 22:06:47 -0400

    Changed import to from statement

--- conftest.py
+++ conftest.py
@@ -21,7 +21,7 @@ def checkmate_board() -> Board:
     return board
 
 
-@pytest.fixture
+@fixture
 def stalemate_board() -> Board:
     """Returns a board with a stalemate position."""
     return Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
