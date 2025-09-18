b78559d77bda8fbe8abc2186b4d34bb854c19866
dhodgson615 <dhodgson0615@gmail.com>
2025-09-18 13:54:31 -0400

    Converted import to from statement

--- test_engine_handler.py
+++ test_engine_handler.py
@@ -42,7 +42,7 @@ def test_get_engine(engine_path: str) -> None:
         engine.quit()
 
 
-@pytest.mark.skipif(PATH_NOT_FOUND, reason=PATH_NOT_FOUND_MSG)
+@mark.skipif(PATH_NOT_FOUND, reason=PATH_NOT_FOUND_MSG)
 def test_evaluate_move(engine_path: str) -> None:
     """Test that evaluate_move returns a move and score."""
     engine = get_engine(engine_path)
