8f5cf65d0c073f1e60d5acc09a0889249b21c8f0
dhodgson615 <dhodgson0615@gmail.com>
2025-09-18 13:59:18 -0400

    Refactor

--- test_tablebase.py
+++ test_tablebase.py
@@ -4,50 +4,42 @@ from pytest import skip
 from engine_handler import evaluate_move, get_engine, get_syzygy_tablebase
 
 
-def test_tablebase_functionality() -> (
-    None
-):  # TODO: make this function use less nesting
+def test_tablebase_functionality() -> None:
     """Test basic functionality of Syzygy tablebase integration."""
-    # Load tablebase
     tablebase = get_syzygy_tablebase()
 
     if not tablebase:
         skip("Tablebase not found or loaded. Check installation.")
 
-    # Create a 3-piece endgame position (King vs King + Queen)
-    board = Board("8/8/8/8/8/2k5/8/K3q3 w - - 0 1")
-
     try:
-        # Get WDL info directly from tablebase
+        board = Board("8/8/8/8/8/2k5/8/K3q3 w - - 0 1")
         wdl = tablebase.get_wdl(board)
 
-        # If we can't probe this position, try a simpler one
         if wdl is None:
-            board = Board("8/8/8/8/8/2k5/8/K7 w - - 0 1")  # Just kings
+            board = Board("8/8/8/8/8/2k5/8/K7 w - - 0 1")
             wdl = tablebase.get_wdl(board)
 
         assert wdl is not None, "WDL value should not be None"
         assert isinstance(wdl, int), "WDL should be an integer"
 
-        # Test with engine evaluation if possible
-        if wdl is not None:
-            engine = get_engine()
+        engine = get_engine()
 
-            try:
-                # Test a move evaluation using tablebase
-                moves = list(board.legal_moves)
+        try:
+            moves = list(board.legal_moves)
 
-                if moves:  # Make sure there are legal moves
-                    move = moves[0]
+            if not moves:
+                return
 
-                    move_obj, (_, mate) = evaluate_move(
-                        board, engine, move, tablebase=tablebase
-                    )
+            move = moves[0]
 
-                    assert move_obj is not None, "Move should not be None"
+            move_obj, (_, mate) = evaluate_move(
+                board, engine, move, tablebase=tablebase
+            )
 
-            finally:
-                engine.quit()
+            assert move_obj is not None, "Move should not be None"
+
+        finally:
+            engine.quit()
 
     finally:
         tablebase.close()
