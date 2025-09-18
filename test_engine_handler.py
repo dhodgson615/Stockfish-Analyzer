9d29afc21bc4aca771d95264503f851bd8d0638d
dhodgson615 <dhodgson0615@gmail.com>
2025-09-18 13:54:23 -0400

    Converted import to from statement

--- test_engine_handler.py
+++ test_engine_handler.py
@@ -20,7 +20,7 @@ PATH_NOT_FOUND = not path.exists(STOCKFISH_PATH)
 PATH_NOT_FOUND_MSG = "Stockfish engine not found"
 
 
-@pytest.mark.skipif(PATH_NOT_FOUND, reason=PATH_NOT_FOUND_MSG)
+@mark.skipif(PATH_NOT_FOUND, reason=PATH_NOT_FOUND_MSG)
 def test_get_engine(engine_path: str) -> None:
     """Test that the engine can be initialized and quit properly."""
     engine = get_engine(engine_path)
