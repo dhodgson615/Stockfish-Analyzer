4c22bb6ceb0fc45fc8c274577c63699bcb42b797
dhodgson615 <dhodgson0615@gmail.com>
2025-09-19 22:29:13 -0400

    Refactored all `from x import y` statements to `import x` statements

--- test/test_integration.py
+++ test/test_integration.py
@@ -49,7 +49,10 @@ def test_play_game_simple_sequence(
     move_history: list[chess.Move] = []
 
     # Mock handle_user_input to return a checkmate sequence
-    moves = [from_uci(m) for m in ["f1c4", "f8c5", "d1f3", "g7g6", "f3f7"]]
+    moves = [
+        src.input_handler.from_uci(m)
+        for m in ["f1c4", "f8c5", "d1f3", "g7g6", "f3f7"]
+    ]
     move_iter = iter(moves)
 
     def mock_handle_input(_: Board) -> Move | None:
