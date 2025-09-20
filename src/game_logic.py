d96f1cb1a1fbba1aeddbae4af360f72d6ad0f398
dhodgson615 <dhodgson0615@gmail.com>
2025-09-20 13:33:43 -0400

    Formatting

--- src/game_logic.py
+++ src/game_logic.py
@@ -53,7 +53,10 @@ def evaluate_and_show_moves(
         board_ui.print_tablebase_info(board, tablebase)
 
     moves_eval = engine_handler.get_move_evals(
-        board, engine, depth=engine_handler.get_dynamic_eval_depth(board), tablebase=tablebase
+        board,
+        engine,
+        depth=engine_handler.get_dynamic_eval_depth(board),
+        tablebase=tablebase,
     )
 
     eval_time = time.time() - start_time
