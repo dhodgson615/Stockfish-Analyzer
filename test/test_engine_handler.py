3dd22f263db1275b44c1e136c1c84e11ac534841
dhodgson615 <dhodgson0615@gmail.com>
2025-09-19 22:25:06 -0400

    Refactored all `from x import y` statements to `import x` statements

--- test/test_engine_handler.py
+++ test/test_engine_handler.py
@@ -142,7 +142,9 @@ def test_get_engine_invalid_path() -> None:
 
 def test_try_tablebase_evaluation_win() -> None:
     """Test tablebase evaluation for a winning position."""
-    board = Board("8/8/8/8/8/K7/8/k1q5 w - - 0 1")  # White to move, losing
+    board = chess.Board(
+        "8/8/8/8/8/K7/8/k1q5 w - - 0 1"
+    )  # White to move, losing
 
     mock_tablebase = MagicMock()
     mock_tablebase.get_wdl.return_value = -2  # Loss for white
