af5bb0f8f788e5e61b56638d582877d7830c8bc5
dhodgson615 <dhodgson0615@gmail.com>
2025-09-20 13:34:04 -0400

    Formatting

--- test/test_engine_handler.py
+++ test/test_engine_handler.py
@@ -275,7 +275,7 @@ def test_get_dynamic_eval_depth_middlegame() -> None:
     """Test dynamic depth calculation for middlegame-like positions."""
     # Test opening position (move 8, many pieces)
     board = chess.Board()
-    
+
     # Simulate moves - this is still opening according to our logic
     moves = ["e4", "e5", "Nf3", "Nc6", "Bb5", "a6", "Ba4", "Nf6", 
              "O-O", "Be7", "Re1", "b5", "Bb3", "d6", "c3", "O-O"]
