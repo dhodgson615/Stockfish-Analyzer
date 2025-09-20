4c54d086627eeb5303fbc758a14851792f56ca11
dhodgson615 <dhodgson0615@gmail.com>
2025-09-20 00:56:00 -0400

    Changed import structure

--- src/engine_handler.py
+++ src/engine_handler.py
@@ -164,7 +164,7 @@ def get_move_evals(
 
         moves_evaluations[move_obj] = score_data
 
-        src.board_ui.display_progress(
+        board_ui.display_progress(
             i,
             len(list(board.legal_moves)),
             start_time,
