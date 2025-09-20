3daafeb4b9515ecb02fdfdef23e189200a4f1884
dhodgson615 <dhodgson0615@gmail.com>
2025-09-19 22:22:31 -0400

    Refactored all `from x import y` statements to `import x` statements

--- test/test_board_ui.py
+++ test/test_board_ui.py
@@ -98,7 +98,9 @@ def test_show_mate_info() -> None:
 
 def test_print_game_over_info(checkmate_board: Board) -> None:
     """Test print_game_over_info function."""
-    move_history = [Move.from_uci(m) for m in ["f2f3", "e7e5", "g2g4", "d8h4"]]
+    move_history = [
+        chess.Move.from_uci(m) for m in ["f2f3", "e7e5", "g2g4", "d8h4"]
+    ]
 
     with StringIO() as buf, redirect_stdout(buf):
         print_game_over_info(checkmate_board, move_history)
