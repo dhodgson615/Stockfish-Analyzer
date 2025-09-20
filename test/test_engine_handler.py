b5900d68bf8e72a3035706261a5eca19c547d071
dhodgson615 <dhodgson0615@gmail.com>
2025-09-20 15:07:57 -0400

    Spacing

--- test/test_engine_handler.py
+++ test/test_engine_handler.py
@@ -201,6 +201,7 @@ def test_get_engine_evaluation() -> None:
     mock_score.white.return_value.score.return_value = 900000
     mock_score.white.return_value.mate.return_value = 3
     mock_engine.analyse.return_value = {"score": mock_score}
+
     score, mate = src.engine_handler.get_engine_evaluation(
         board, mock_engine, src.engine_handler.EVAL_DEPTH
     )
