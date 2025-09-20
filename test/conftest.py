9653e14a4bd9f1c38995cee7ba03b8ea108cc075
dhodgson615 <dhodgson0615@gmail.com>
2025-09-19 22:21:04 -0400

    Refactored all `from x import y` statements to `import x` statements

--- test/conftest.py
+++ test/conftest.py
@@ -26,7 +26,7 @@ def checkmate_board() -> chess.Board:
 @pytest.fixture
 def stalemate_board() -> chess.Board:
     """Returns a board with a stalemate position."""
-    return Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
+    return chess.Board("8/8/8/8/8/5k2/7p/7K w - - 0 1")
 
 
 @fixture
